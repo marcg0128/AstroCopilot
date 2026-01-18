85da158abfabb09de2b6dde6ea18ee7cd83d4ffc
Marcos Gomez Alvarez <83846095+marcg0128@users.noreply.github.com>
2026-01-18 18:00:21 +0100

    Downloaded a background and added it to page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,30 @@
+import NavBar from "@/app/components/NavBar";
+import Galaxy from "@/components/Galaxy";
 
 
 export default function Home() {
     return (
-      <></>
+        <>
+            <div className="w-full h-screen relative overflow-hidden">
+                <div className="relative z-10">
+                    <NavBar/>
+                </div>
+                <div className="absolute inset-0 z-0">
+                    <Galaxy
+                        starSpeed={0.5}
+                        density={0.7}
+                        hueShift={0}
+                        speed={0.1}
+                        glowIntensity={0.2}
+                        saturation={0.25}
+                        mouseRepulsion={false}
+                        repulsionStrength={2}
+                        twinkleIntensity={0.3}
+                        rotationSpeed={0.1}
+                        transparent={false}
+                    />
+                </div>
+            </div>
+        </>
     );
 }
