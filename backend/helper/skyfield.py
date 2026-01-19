4b9b0cc72d73e5a6cbaea9aa3d67a663de1fa9fe
Marcos Gomez Alvarez <83846095+marcg0128@users.noreply.github.com>
2026-01-19 10:18:27 +0100

    Created a function with skyfield api that calculates the coordinates of the satellite based of the TLE data

---
+++ backend/helper/skyfield.py
@@ -0,0 +1,15 @@
+from skyfield.api import EarthSatellite, load, wgs84
+
+
+def get_coordinates(tle_data):
+    satellite = EarthSatellite(tle_data["line1"], tle_data["line2"], tle_data["name"])
+    geocentric = satellite.at(load.timescale().now())
+
+
+    lat, lon, height = wgs84.geographic_position_of(geocentric)
+
+    return {
+        "lat": lat.degrees,
+        "lon": lon.degrees,
+        "height": height
+    }
