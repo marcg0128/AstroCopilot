d62ed3565c8906386342a3ab477a589566bacb2d
Marcos Gomez Alvarez <83846095+marcg0128@users.noreply.github.com>
2026-01-20 17:16:49 +0100

    get lat, lon and height

--- backend/helper/skyfield.py
+++ backend/helper/skyfield.py
@@ -5,6 +5,7 @@ def get_coordinates(tle_data):
     satellite = EarthSatellite(tle_data["line1"], tle_data["line2"], tle_data["name"])
     geocentric = satellite.at(load.timescale().now())
 
+    geo_pos = wgs84.geographic_position_of(geocentric)
 
     lat, lon, height = wgs84.geographic_position_of(geocentric)
 
