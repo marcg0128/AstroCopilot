35efd98da4c2b6c278072e1e4db167bf2a1ca209
Marcos Gomez Alvarez <83846095+marcg0128@users.noreply.github.com>
2026-01-20 18:29:19 +0100

    add database session management for satellite data

---
+++ backend/database/satellitesDatabase.py
@@ -0,0 +1,59 @@
+import os
+import mysql.connector
+import time
+
+class DatabaseSeassion:
+    def __init__(self):
+        self.connection = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            port=int(os.getenv("DB_PORT")),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
+        )
+        self.cursor = self.connection.cursor(dictionary=True)
+
+    def fetch_all_satellites(self):
+        query = "SELECT norad_id, name FROM satellites"
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
+
+    def get_satellite_data(self, norad_id):
+        query = "SELECT * FROM satellites WHERE norad_id = %s"
+        self.cursor.execute(query, (norad_id,))
+        return self.cursor.fetchone()
+
+    def get_satellites_data(self, norad_ids):
+        format_strings = ','.join(['%s'] * len(norad_ids))
+        query = f"SELECT * FROM satellites WHERE norad_id IN ({format_strings})"
+        self.cursor.execute(query, tuple(norad_ids))
+        return self.cursor.fetchall()
+
+    def add_satellite_data(self, norad_id, name, latitude, longitude, path):
+        query = """
+            INSERT INTO satellites (norad_id, name, lat, lon, path, last_update)
+            VALUES (%s, %s, %s, %s, %s, NOW())
+        """
+        self.cursor.execute(query, (norad_id, name, latitude, longitude, path))
+        self.connection.commit()
+
+    def update_satellite_data(self, norad_id, name, lat, lon, height, path):
+        query = """
+            UPDATE satellites
+            SET lat = %s, lon = %s, height = %s, path = %s, last_update = NOW()
+            WHERE norad_id = %s
+        """
+
+        if not self.get_satellite_data(norad_id):
+            print(f"Satellite with NORAD ID {norad_id} not found in database. Adding new entry.")
+            self.add_satellite_data(norad_id, name, lat, lon, path)
+            return
+
+        self.cursor.execute(query, (lat, lon, height, path, norad_id))
+        self.connection.commit()
+
+
+
+    def close(self):
+        self.cursor.close()
+        self.connection.close()
